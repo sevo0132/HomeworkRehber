680cf494a07caf8e117a60cd9684b3d0ab8f7ea1
okanaydin <okanaydin1994@gmail.com>
2017-04-03 18:04:10 +0300

    Firebase Authentication is completed.

---
+++ app/src/main/java/com/example/okanaydin/homeworkrehber/FirebaseAuthentication/UyeOl.java
@@ -0,0 +1,103 @@
+package com.example.okanaydin.homeworkrehber.FirebaseAuthentication;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.okanaydin.homeworkrehber.MainActivity;
+import com.example.okanaydin.homeworkrehber.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UyeOl extends AppCompatActivity {
+
+
+    private EditText uyeEmail,uyeParola;
+    private Button yeniUyeButton,uyeGirisButton;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uye_ol);
+
+        //FirebaseAuth sınıfının referans olduğu nesneleri kullanmak için getInstance methodunu kullanıyoruz.
+        auth=FirebaseAuth.getInstance();
+
+        uyeEmail=(EditText)findViewById(R.id.uyeEmail);
+        uyeParola=(EditText)findViewById(R.id.uyeParola);
+        yeniUyeButton=(Button)findViewById(R.id.yeniUyeButton);
+        uyeGirisButton=(Button)findViewById(R.id.uyeGirisButton);
+
+        yeniUyeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email=uyeEmail.getText().toString();
+                String parola=uyeParola.getText().toString();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(),"Mail adresinizi giriniz",Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(parola)){
+                    Toast.makeText(getApplicationContext(),"Parola giriniz",Toast.LENGTH_LONG).show();
+                }
+
+                if (parola.length()<8){
+                    Toast.makeText(getApplicationContext(),"Parola en az 8 karakterden oluşmalıdır",Toast.LENGTH_LONG).show();
+                }
+
+
+                //FirebaseAuth ile email,parola parametrelerini kullanarak yeni bir kullanıcı oluşturuyoruz.
+                auth.createUserWithEmailAndPassword(email,parola)
+                        .addOnCompleteListener(UyeOl.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                                //İşlem başarısız olursa kullanıcıya bir Toast mesajıyla bildiriyoruz.
+                                if (!task.isSuccessful()) {
+
+                                    Toast.makeText(getApplicationContext(), "Yetkilendirme Hatası", Toast.LENGTH_LONG).show();
+                                }
+
+                                //İşlem başarılı ise  MainActivity e yönlendiriyoruz.
+                                else {
+                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                    finish();
+                                }
+
+                            }
+                        });
+
+            }
+        });
+
+
+        uyeGirisButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(),Giris.class));
+                finish();
+
+            }
+        });
+
+
+
+
+
+    }
+}
+
--- app/src/main/java/com/example/okanaydin/homeworkrehber/MainActivity.java
+++ app/src/main/java/com/example/okanaydin/homeworkrehber/MainActivity.java
@@ -1,13 +1,119 @@
 package com.example.okanaydin.homeworkrehber;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.okanaydin.homeworkrehber.FirebaseAuthentication.Giris;
+import com.example.okanaydin.homeworkrehber.FirebaseAuthentication.UyeOl;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private Button kullaniciSil, cikisYap;
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kullaniciSil=(Button)findViewById(R.id.kullaniciSil);
+        cikisYap=(Button)findViewById(R.id.cikis_yap);
+        text=(TextView)findViewById(R.id.yazi);
+
+
+        //FirebaseAuth sınıfının referans olduğu nesneleri kullanabilmek için getInstance metodunu kullanıyoruz.
+        auth = FirebaseAuth.getInstance();
+
+        //Bir AuthStateListener dinleyicisi oluşturuyoruz ve bu dinleyici onAuthStateChanged bölümünü çalıştırır.
+        // Buradaki getCurrentUser metodu ile kullanıcı verilerine ulaşabilmekteyiz.
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                // Eğer geçerli bir kullanıcı oturumu yoksa LoginActivity e geçilir.
+                // Oturum kapatma işlemi yapıldığında LoginActivity e geçilir.
+
+                if (user == null) {
+
+                    startActivity(new Intent(getApplicationContext(), Giris.class));
+                    finish();
+                }
+
+
+
+            }
+        };
+
+        //getCurrentUser metodu üzerinden ulaştığımız kullanıcı verilerinde getEmail ile de kullanıcının mailini kullanarak,
+        // kullanıcıya bir text gösteriyoruz.
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        text.setText("Merhaba, " + user.getEmail());
+
+        kullaniciSil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (user != null){
+                    user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            //Silme işlemi başarılı oldugunda kullanıcıya bir mesaj gösterilip UyeOlActivity e geçiliyor.
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Hesabın silindi.Yeni bir hesap oluştur!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), UyeOl.class));
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(),"İşlem başarısız oldu",Toast.LENGTH_SHORT);
+                            }
+                        }
+                    });
+                }
+
+            }
+
+
+        });
+
+        cikisYap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //FirebaseAuth.getInstance().signOut ile oturumu kapatabilmekteyiz.
+                auth.signOut();
+
+                startActivity(new Intent(getApplicationContext(), Giris.class));
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            auth.removeAuthStateListener(authStateListener);
+        }
     }
 }
+
